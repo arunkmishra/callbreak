9d39453828325379135b2148d39608c3b43fc341
Arun Kumar Mishra <arunkumarmishra@Aruns-MacBook-Pro.local>
2026-05-30 02:25:12 +0700

    fix: bypass audioplayers web bug by reading rootBundle into BytesSource

--- callbreak-client/lib/core/audio_service.dart
+++ callbreak-client/lib/core/audio_service.dart
@@ -1,40 +1,44 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/services.dart' show rootBundle;
 import 'package:audioplayers/audioplayers.dart';
 
 class AudioService {
   static AudioPlayer? _player;
   static bool _isEnabled = true;
+  static AudioPlayer? _sequencePlayer;
 
-  static final Source _tingSource = AssetSource('sounds/ting.wav');
-  static final Source _cardDealSequenceSource = AssetSource('sounds/card_deal_sequence.wav');
+  static Uint8List? _tingBytes;
+  static Uint8List? _sequenceBytes;
 
   static Future<void> playTurnAlert() async {
-    print('AudioService: playTurnAlert requested. isEnabled=$_isEnabled');
     if (!_isEnabled) return;
     try {
       _player ??= AudioPlayer();
-      print('AudioService: player instantiated.');
       if (_player!.state == PlayerState.playing) {
         await _player!.stop();
       }
-      print('AudioService: calling play() on ting.wav');
-      await _player!.play(_tingSource);
-      print('AudioService: play() completed');
+      if (kIsWeb && _tingBytes != null) {
+        await _player!.play(BytesSource(_tingBytes!));
+      } else if (!kIsWeb) {
+        await _player!.play(AssetSource('sounds/ting.wav'));
+      }
     } catch (e) {
       print('AudioService: ERROR playing sound: $e');
     }
   }
 
-  static AudioPlayer? _sequencePlayer;
-
   static Future<void> playCardDealSequence() async {
     if (!_isEnabled) return;
-    
     try {
       _sequencePlayer ??= AudioPlayer();
       if (_sequencePlayer!.state == PlayerState.playing) {
         await _sequencePlayer!.stop();
       }
-      await _sequencePlayer!.play(_cardDealSequenceSource);
+      if (kIsWeb && _sequenceBytes != null) {
+        await _sequencePlayer!.play(BytesSource(_sequenceBytes!));
+      } else if (!kIsWeb) {
+        await _sequencePlayer!.play(AssetSource('sounds/card_deal_sequence.wav'));
+      }
     } catch (e) {
       print('AudioService: ERROR playing sequence: $e');
     }
@@ -44,18 +48,29 @@ class AudioService {
     if (!_isEnabled) return;
     try {
       // Force Android/iOS to treat this as a Game audio source (plays over media volume)
-      await AudioPlayer.global.setAudioContext(AudioContextConfig(
-        respectSilence: false,
-        stayAwake: false,
-      ).build());
+      if (!kIsWeb) {
+        await AudioPlayer.global.setAudioContext(AudioContextConfig(
+          respectSilence: false,
+          stayAwake: false,
+        ).build());
+      }
 
       _player ??= AudioPlayer();
-      await _player!.setSource(_tingSource);
-      print('AudioService: preloaded ting.wav');
-
       _sequencePlayer ??= AudioPlayer();
-      await _sequencePlayer!.setSource(_cardDealSequenceSource);
-      print('AudioService: preloaded card_deal_sequence.wav');
+
+      if (kIsWeb) {
+        final tingData = await rootBundle.load('assets/sounds/ting.wav');
+        _tingBytes = tingData.buffer.asUint8List();
+        await _player!.setSource(BytesSource(_tingBytes!));
+
+        final seqData = await rootBundle.load('assets/sounds/card_deal_sequence.wav');
+        _sequenceBytes = seqData.buffer.asUint8List();
+        await _sequencePlayer!.setSource(BytesSource(_sequenceBytes!));
+      } else {
+        await _player!.setSource(AssetSource('sounds/ting.wav'));
+        await _sequencePlayer!.setSource(AssetSource('sounds/card_deal_sequence.wav'));
+      }
+      print('AudioService: preloaded ting.wav and card_deal_sequence.wav');
     } catch (e) {
       print('AudioService: ERROR preloading sound: $e');
     }
